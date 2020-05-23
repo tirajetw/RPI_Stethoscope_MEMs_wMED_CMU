20aa46b2bb4b31062f549017f6a1cd178bf2955f
tirajetw <tirajetw@gmail.com>
2020-05-23 22:13:04 +0700

    first commit

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ myUsername = "jet"
 myPassword = "s4324$"
 tz = pytz.timezone('Asia/Bangkok')
 button = Button(17)
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None 
 
 while True:
     if button.is_pressed:
@@ -20,7 +22,7 @@ while True:
         os.system('arecord -D dmic_sv -c2 -r 48000 -f S32_LE -t wav -V mono -v {}.wav -d 30'.format(filename))
 
         #  sftp file to server
-        with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
+        with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, cnopts=cnopts) as sftp:
             print ("Connection succesfully stablished ... ")
             localFilePath = '{}.wav'.format(filename)
             remoteFilePath = 'RPI_Stethoscope_MEMs_wMED_CMU/recfile/{}.wav'.format(filename)
